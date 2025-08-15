077c522edaea6d97310ed843a7d712a45cec97b1
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-08-15 21:29:52 +0530

    learning management system

---
+++ backend/model/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "educator"],
+    },
+    password:{
+     type:String,
+
+    },
+    photoUrl:{
+     type:String,
+     default:""
+    },
+    enrolledCourses:[{
+     type:mongoose.Schema.Types.ObjectId,
+     ref:"Course"
+    }]
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User",userSchema)
+export default(User)
