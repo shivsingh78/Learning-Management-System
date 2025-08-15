9be7b530a672ea16b5420b273a33e1f6d25138bd
shivsingh78 <shivsinghbaghel95@gmail.com>
2025-08-15 23:42:47 +0530

    add signUp function for user registration with validation

---
+++ backend/controller/authController.js
@@ -0,0 +1,22 @@
+import User from "../model/userModel.js"
+import validator from "validator"
+import bcrypt from "bcryptjs"
+
+export const signUp = async (req,res){
+     try {
+          const {name, email, password, role} = req.body;
+          let existUser = await User.findOne({email})
+          if(existUser){
+               return res.status(400).json({message: "User is already Exist"})
+          }
+          if(!validator.isEmail(email)){
+               return res.status(400).json({message: "Enter Valid email"})
+          }
+          if(password.length < 8){
+               return res.status(400).json({message:"Enter Strong password"})
+          }
+          
+     } catch (error) {
+          
+     }
+}
